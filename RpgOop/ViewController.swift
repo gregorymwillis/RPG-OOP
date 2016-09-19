b16725f2dd3f77b2cedbdb4f6877e0c14095c043
Greg Willis <gregorymwillis@gmail.com>
2016-09-19 12:32:22 -0600

    Initial Commit

---
+++ RpgOop/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RpgOop
+//
+//  Created by Greg Willis on 9/19/16.
+//  Copyright © 2016 Willis Programming. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
